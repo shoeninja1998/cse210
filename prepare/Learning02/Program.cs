01e2e6c77c09fc66ab5a34dcc00647c6fd01b9f9
shoeninja1998 <moerani@byui.edu>
2024-01-18 13:04:54 -0700

    Completed Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new(){
+            _jobTitle = "CEO",
+            _company = "Microsoft",
+            _startYear = 1998,
+            _endYear = 2011
+        };
+        Job job2 = new(){
+            _jobTitle = "Owner",
+            _company = "Apple",
+            _startYear = 2011,
+            _endYear = 2024
+        };
+        Resume resume1 = new()
+        {
+            _name = "Eric Johnson"
+        };
+
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.DisplayResume();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,13 @@
+public class Resume{
+    public string _name;
+    public List<Job> _jobs = new();
+
+    public void DisplayResume(){
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+        
+        foreach (Job job in _jobs){
+            job.DisplayJob();
+        }
+    }
+}
